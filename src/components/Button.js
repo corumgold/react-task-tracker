5dc837e140f9b99a7759a85932f40d7e7f83a8c2
Corum Gold <goldcorum@gmail.com>
2022-08-19 08:28:07 -0400

    Add button component

---
+++ src/components/Button.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Button = (props) => {
+  return (
+    <button style={{ backgroundColor: props.color }} className="btn">
+      {props.text}
+    </button>
+  );
+};
+
+export default Button;
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,14 @@
 //rafce snippet
 import React from "react";
 import PropTypes from "prop-types";
+import Button from "./Button";
 
 //props brings in properties from App.js
 const Header = (props) => {
   return (
-    <header>
+    <header className="header">
       <h1>{props.title}</h1>
+      <Button color='green' text='Hello' />
     </header>
   );
 };
