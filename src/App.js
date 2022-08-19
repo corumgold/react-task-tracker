8b3650957f0f874e26f82894d150cdaf31eae061
Corum Gold <goldcorum@gmail.com>
2022-08-19 08:56:40 -0400

    Add tasks to our global state

--- src/App.js
+++ src/App.js
@@ -1,9 +1,36 @@
 import Header from "./components/Header";
+import Tasks from "./components/Tasks";
+import React from "react";
 
 function App() {
+  //we want to 'use state' with these tasks
+  //state is immutable, so we need to use setTasks to change
+  //This is in our GLOBAL STATE
+  const [tasks, setTasks] = React.useState([
+    {
+      id: 1,
+      text: "Doc Appt",
+      day: "Feb 5th at 2:30pm",
+      reminder: true,
+    },
+    {
+      id: 2,
+      text: "School Meeting",
+      day: "Feb 6th at 12:30pm",
+      reminder: true,
+    },
+    {
+      id: 3,
+      text: "Food Shopping",
+      day: "Feb 5th at 2:30pm",
+      reminder: false,
+    },
+  ]);
+
   return (
     <div className="container">
       <Header />
+      <Tasks tasks={tasks} />
     </div>
   );
 }
--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -1,28 +1,14 @@
 import React from "react";
 
-const tasks = [
-  {
-    id: 1,
-    text: "Doc Appt",
-    day: "Feb 5th at 2:30pm",
-    reminder: true,
-    },
-    {
-        id: 2,
-        text: "School Meeting",
-        day: "Feb 6th at 12:30pm",
-        reminder: true,
-    },
-    {
-        id: 3,
-        text: "Food Shopping",
-        day: "Feb 5th at 2:30pm",
-        reminder: false,
-      },
-];
-
-const Tasks = () => {
-  return <div>Tasks</div>;
+const Tasks = ({ tasks }) => {
+  return (
+    <>
+      {tasks.map((task) => (
+        //this is called a 'list' and the items need unique keys!
+        <h3 key={task.id}>{task.text}</h3>
+      ))}
+    </>
+  );
 };
 
 export default Tasks;
